342c565499f32f314fd1234e739310329d1612ce
Pablo Queiroz <pablo.hl2@gmail.com>
2026-03-25 11:58:46 +0000

    feat(backend): attachments models created

--- grftalk-backend/attachments/admin.py
+++ grftalk-backend/attachments/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from attachments.models import FileAttachments, AudioAttachments
+
+admin.site.register(FileAttachments)
+admin.site.register(AudioAttachments)
--- grftalk-backend/attachments/models.py
+++ grftalk-backend/attachments/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class FileAttachments(models.Model):
+    name = models.CharField(max_length=90)
+    extension = models.CharField(max_length=15)
+    size = models.FloatField()
+    src = models.TextField()
+    content_type = models.CharField(max_length=45)
+
+    class Meta:
+        db_table = 'file_attachments'
+
+
+class AudioAttachments(models.Model):
+    src = models.TextField()
+
+    class Meta:
+        db_table = 'audio_attachments'
