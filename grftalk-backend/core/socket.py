e32cde4a896c7f9e9e654d740ec29212349d13be
Pablo Queiroz <pablo.hl2@gmail.com>
2026-03-27 14:12:06 +0000

    feat(socket): client events

--- grftalk-backend/core/socket.py
+++ grftalk-backend/core/socket.py
@@ -1,8 +1,48 @@
 import socketio
 
 from django.conf import settings
-# Create a Socket.Io Server
+from django.utils.timezone import now
 
+from chats.models import Chat, ChatMessage
+
+
+# Create a Socket.Io Server
 socket = socketio.Server(
     cors_allowed_origins=settings.CORS_ALLOWED_ORIGINS
 )
+
+
+@socket.event
+def update_messages_as_seen(sid, data):
+    chat_id = data.get('chat_id')
+
+    # Getting chat
+    chat = Chat.objects.values(
+        'from_user_id',
+        'to_user_id'
+    ).filter(
+        id=chat_id
+    ).first()
+
+    # Updating chat messages as viewed
+    ChatMessage.objects.filter(
+        chat_id=chat_id,
+        viewed_at__isnull=True
+    ).update(
+        viewed_at=now()
+    )
+
+    # Sending update chat to users
+    socket.emit('update_chat', {
+        'query': {
+            'users': [chat['from_user_id'], chat['to_user_id']]
+        }
+    })
+
+    # Update chat message as seen
+    socket.emit('mark_message_as_seen', {
+        'query': {
+            'chat_id': chat_id,
+            'exclude_user_id': data.get('exclude_user_id')
+        }
+    })
