156343a6288f8c6684bb9082d86a94fab57feb97
Pablo Queiroz <pablo.hl2@gmail.com>
2026-04-02 14:47:46 +0100

    feat: create authStore with Zustand

---
+++ grftalk-frontend/src/stores/authStore.ts
@@ -0,0 +1,19 @@
+import { create } from "zustand";
+import { User } from "../types/User";
+
+export type AuthState = {
+  user: User | null;
+};
+
+export type AuthActions = {
+  setUser: (user: User) => void;
+  clearUser: () => void;
+};
+
+export type AuthStore = AuthState & AuthActions;
+
+export const useAuthStore = create<AuthStore>((set) => ({
+  user: null,
+  setUser: (user) => set({ user }),
+  clearUser: () => set({ user: null }),
+}));
