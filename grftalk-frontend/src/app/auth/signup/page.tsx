be88ab94fb488e36ffe8beffbb020d5ba1c437f3
Pablo Queiroz <pablo.hl2@gmail.com>
2026-04-08 15:57:18 +0100

    feat(auth): add signup page and layout utility

---
+++ grftalk-frontend/src/app/auth/signup/page.tsx
@@ -0,0 +1,10 @@
+import SignUpPage from "@/src/components/Pages/SignUp";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sign Up",
+};
+
+const SignUp = () => <SignUpPage />;
+
+export default SignUp;
--- grftalk-frontend/src/components/Pages/SignUp.tsx
+++ grftalk-frontend/src/components/Pages/SignUp.tsx
@@ -6,11 +6,21 @@ import { useAuthStore } from "@/src/stores/authStore";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import { toast } from "sonner";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardTitle,
+} from "@/src/components/ui/card";
+import { Button } from "../ui/button";
+import { Skeleton } from "../ui/skeleton";
+import { Field, FieldError, FieldLabel } from "../ui/field";
+import { Input } from "../ui/input";
 
-const SignUp = () => {
-  const [Loading, setLoading] = useState(false);
+const SignUpPage = () => {
+  const [loading, setLoading] = useState(false);
 
   const setUser = useAuthStore((state) => state.setUser);
   const router = useRouter();
@@ -41,6 +51,90 @@ const SignUp = () => {
     router.push("/");
   };
 
-  return <div>SignUp</div>;
+  return (
+    <main className="h-app flex items-center justify-center overflow-auto px-6">
+      <Card className="w-96">
+        <CardTitle className="m-auto py-2 text-xl">Create a account</CardTitle>
+        <CardDescription className="m-auto w-87.5 text-center">
+          Insert your name, email and password to create a account.
+        </CardDescription>
+
+        <CardContent>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="space-y-3">
+              {loading ? (
+                Array.from({ length: 3 }, (_, key) => (
+                  <Skeleton key={key} className="h-10 rounded-md" />
+                ))
+              ) : (
+                <>
+                  <Controller
+                    name="name"
+                    control={form.control}
+                    render={({ field, fieldState }) => (
+                      <Field data-invalid={fieldState.invalid}>
+                        <FieldLabel htmlFor="name">Name</FieldLabel>
+                        <Input
+                          {...field}
+                          id="name"
+                          type="text"
+                          placeholder="Your name..."
+                          aria-invalid={fieldState.invalid}
+                        />
+                        {fieldState.invalid && (
+                          <FieldError errors={[fieldState.error]} />
+                        )}
+                      </Field>
+                    )}
+                  />
+
+                  <Controller
+                    name="email"
+                    control={form.control}
+                    render={({ field, fieldState }) => (
+                      <Field data-invalid={fieldState.invalid}>
+                        <FieldLabel htmlFor="email">Email</FieldLabel>
+                        <Input
+                          {...field}
+                          id="email"
+                          type="email"
+                          placeholder="johndoe@email.com"
+                          aria-invalid={fieldState.invalid}
+                        />
+                        {fieldState.invalid && (
+                          <FieldError errors={[fieldState.error]} />
+                        )}
+                      </Field>
+                    )}
+                  />
+                  <Controller
+                    name="password"
+                    control={form.control}
+                    render={({ field, fieldState }) => (
+                      <Field data-invalid={fieldState.invalid}>
+                        <FieldLabel htmlFor="password">Password</FieldLabel>
+                        <Input
+                          {...field}
+                          id="password"
+                          type="password"
+                          placeholder="********"
+                          aria-invalid={fieldState.invalid}
+                        />
+                        {fieldState.invalid && (
+                          <FieldError errors={[fieldState.error]} />
+                        )}
+                      </Field>
+                    )}
+                  />
+                </>
+              )}
+            </div>
+
+            <Button disabled={loading}>Register</Button>
+          </form>
+        </CardContent>
+      </Card>
+    </main>
+  );
 };
-export default SignUp;
+export default SignUpPage;
