e57a3a9209abd9f4ccc9bfdcd077ab86a0071e10
Pablo Queiroz <pablo.hl2@gmail.com>
2026-04-08 13:13:35 +0100

    fix(style): fix styles in header.tsx

--- grftalk-frontend/src/components/Layouts/Header.tsx
+++ grftalk-frontend/src/components/Layouts/Header.tsx
@@ -71,7 +71,7 @@ const Header = () => {
 
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <Button variant="outline" size="icon">
+              <Button variant="outline" size="icon" className="relative text-foreground">
                 <Sun className="size-5 scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
                 <Moon className="absolute size-5 scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
                 <span className="sr-only">Toggle theme</span>
